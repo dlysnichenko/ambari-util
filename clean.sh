b8ab7b01528b7764bc3034494928e3a3290bcfd9
root <root@suse.hortonworks.com>
2012-12-15 15:49:04 +0200

    merged

--- clean.sh
+++ clean.sh
@@ -9,10 +9,4 @@ then
 fi
 cd $DIR
 
-#/etc/init.d/postgresql-9.1 stop
-#rm /var/lib/pgsql -rf
-#cp /root/bak-pgsl/pgsql /var/lib/ -rf
-#chown postgres:postgres -R /var/lib/pgsql
-#/etc/init.d/postgresql-9.1 start
-
 exit 0
